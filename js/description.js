eab288f75f128479cdbfce2d53d5caa34f498b75
Eduard <katseduard2002@gmail.com>
2020-09-14 12:02:33 +0300

    animation_links_KATSEDUARD

--- js/description.js
+++ js/description.js
@@ -1,21 +1,13 @@
 let metalVue = new Vue({
     el : "#content-wrapper",
     data : {
-        flag: false,
-        status: "Читати більше"
+        status: "Читати більше",
+        show: false
     },
     methods : {
         showInfo() {
-            if (this.flag === false) {
-                document.getElementById("info-wrapper").style.opacity = "1";
-                this.flag = true;
-                this.status = "Згорнути";
-            }
-            else {
-                document.getElementById("info-wrapper").style.opacity = "0";
-                this.flag = false;
-                this.status = "Читати більше";
-            }
+            this.show = !this.show;
+            this.status = this.show ? "Згорнути" : "Читати більше"
         }
     }
 });
