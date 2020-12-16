0eaff816ddda48e06e0728d1072d0b11eb79322c
Eduard <katseduard2002@gmail.com>
2020-12-16 20:07:22 +0200

    auth

--- account.php
+++ account.php
@@ -1,3 +1,28 @@
+<?php
+        require('db.php');
+        $link = db_connect();
+        if (isset($_POST['name']) && isset($_POST['surname']) && isset($_POST['email'])
+        && isset($_POST['password'])) {
+          $name = trim($_POST['name']);
+          $surname = trim($_POST['surname']);
+          $email = trim($_POST['email']);
+          $phone = trim($_POST['phone']);
+          $password = trim($_POST['password']);
+
+          $query = "INSERT INTO user_data (phone, email, password) VALUES ('$phone', '$email', '$password');";
+          
+          $query .= "INSERT INTO `user` (`name`, `surname`, `id_data`) VALUES ('$name', '$surname', LAST_INSERT_ID());";
+
+          $result = mysqli_multi_query($link, $query);
+
+          if($result) {
+            $smsg = "Registration complete";
+          }
+          else {
+            $fsmsg = "Error occurred";
+          }
+        }
+?>
 <!doctype html>
 <html lang="en">
 
@@ -42,39 +67,50 @@
             </div>
           </div>
         </div>
-        <?php
-        $name = $_POST["name"];
-        $surname = $_POST["surname"];
-        $birthday = $_POST["birthday"];
-
-        if (!empty($name) && !empty($surname) && !empty($birthday)) {
-          echo "Вітаємо, " . $name . " " . $surname . "!<br>";
-          echo "Дата народження: " . $birthday;
-        } else {
-        ?>
-          <div id="content-wrapper" class="content-wrapper">
-            <form action="account.php" method="POST">
-              <div class="form-group">
-                <label for="namefield">Ім'я</label>
-                <input autocomplete="off" type="text" name="name" class="form-control" id="namefield">
-              </div>
-              <div class="form-group">
-                <label for="surnamefield">Прізвище</label>
-                <input autocomplete="off" type="text" name="surname" class="form-control" id="surnamefield">
-              </div>
-              <div class="form-group form-check">
+        <div id="content-wrapper" style="margin-top: 70px !important;" class="content-wrapper">
+          <form action="account.php" method="POST">
+            <h3 align="center">Sign Up</h3>
+            <?php
+              if(isset($smsg)) { ?> <div class="alert alert-success" role="alert"><?= $smsg ?></div>
+                <?
+              }
+              ?>
+              <?php
+              if(isset($fsmsg)) { ?> <div class="alert alert-danger" role="alert"><?= $fsmsg ?></div>
+                <?
+              }
+              ?>
+            <div class="form-group">
+              <label for="namefield">Name</label>
+              <input autocomplete="off" type="text" name="name" class="form-control" required placeholder="Name" id="namefield">
+            </div>
+            <div class="form-group">
+              <label for="surnamefield">Surname</label>
+              <input autocomplete="off" type="text" name="surname" class="form-control" required placeholder="Surname" id="surnamefield">
+            </div>
+            <div class="form-group">
+              <label for="phonefield">Phone (not required)</label>
+              <input autocomplete="off" type="phone" name="phone" class="form-control" placeholder="Phone" id="surnamefield">
+            </div>
+            <div class="form-group">
+              <label for="emailfield">Email</label>
+              <input type="email" name="email" class="form-control" id="emailfield" required placeholder="example@mail.com">
+            </div>
+            <div class="form-group">
+              <label for="passwordfield">Password</label>
+              <input autocomplete="off" type="password" name="password" class="form-control" required placeholder="password" id="passwordfield">
+            </div>
+            <!-- <div class="form-group form-check">
                 <label class="form-check-label" for="datefield">Оберіть свою дату народження</label>
                 <br>
                 <input type="date" name="birthday" class="form-check-input" id="datefield">
                 <br>
                 <br>
-              </div>
-              <button type="submit" class="btn btn-primary">Відправити</button>
-            </form>
-          </div>
-        <?php
-        }
-        ?>
+              </div> -->
+            <button type="submit" class="btn btn-primary">Sign Up</button>
+            <div class="btn-info btn" style="margin-left: 25px;">Sign In</div>
+          </form>
+        </div>
       </div>
     </div>
   </div>
